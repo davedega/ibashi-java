9fda5856203b84314e89180d6cc5401ae51427f0
davedega <dasher_@hotmail.com>
2018-04-06 00:41:30 +0200

    add project

---
+++ app/src/main/java/com/dega/ibashi/IbashiActivity.java
@@ -0,0 +1,13 @@
+package com.dega.ibashi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IbashiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ibashi);
+    }
+}
